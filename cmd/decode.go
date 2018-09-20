02f8379709103d481e473a3c5c391a4b281e38d6
pedro.mendes <pedro.mendes@corp.globo.com>
2018-09-20 09:54:09 -0300

    cmd/decode: extract line formatting to new func

--- cmd/decode.go
+++ cmd/decode.go
@@ -70,19 +70,26 @@ func decode(token string) {
 //	- title : section name
 //	- output: content to be printed
 //  - color: color spec from fatih/color package
-// TODO refactor this crap.
 func Print(title string, output map[string]interface{}, color func(format string, a ...interface{})) {
 	color("\n%v:", title)
 	for key, val := range output {
-		switch val.(type) {
-		case []interface{}:
-			color("\t- %v:\n", key)
-			for i := 0; i < reflect.ValueOf(val).Len(); i++ {
-				strct := reflect.ValueOf(val).Index(i).Interface()
-				color("\t\t- %v\n", strct)
-			}
-		default:
-			color("\t- %v : %v\n", key, val)
+		PrintLine(key, val, color)
+	}
+}
+
+// PrintLine: Output lines, formatting according to its content.
+//	- key : claim name
+//	- val: claim content
+//  - color: color spec from fatih/color package
+func PrintLine(key string, val interface{}, color func(format string, a ...interface{})) {
+	switch val.(type) {
+	case []interface{}:
+		color("\t- %v:\n", key)
+		innerVal := reflect.ValueOf(val)
+		for i := 0; i < innerVal.Len(); i++ {
+			color("\t\t- %v\n", innerVal.Index(i).Interface())
 		}
+	default:
+		color("\t- %v : %v\n", key, val)
 	}
 }
